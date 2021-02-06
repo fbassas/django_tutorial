26cf315de84c9d2b9de6bc48e10578b6329e981d
Frank <fbassas@gmail.com>
2021-02-06 19:02:30 +0100

    amb bootstrap finalitzat

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, re_path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webapp/', include('webapp.urls')),
-    re_path(r'^$', include('personal.urls')),
+    re_path(r'^', include('personal.urls')),
+    re_path(r'^blog/', include('blog.urls')),
 ]
--- mysite/personal/views.py
+++ mysite/personal/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'personal/home.html')
+
+def contact(request):
+    return render(request, 'personal/basic.html', {'content':['Si vols contactar amb mi, sisplau enviam un email','fbassas@gmail.com']})
